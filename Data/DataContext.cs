0f827a0bb639f03915b5e19312a5a01a43d988dc
burakmey <burak_meydan-gs@hotmail.com>
2024-05-15 16:07:19 +0300

    Applying configurations from assembly.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,4 +1,6 @@
-﻿namespace library_automation_back_end.Data
+﻿using System.Reflection;
+
+namespace library_automation_back_end.Data
 {
     public class DataContext : DbContext
     {
@@ -20,21 +22,7 @@
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<BookAuthor>().HasKey(ba => new { ba.BookId, ba.AuthorId });
-            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Book).WithMany(b => b.BookAuthors).HasForeignKey(ba => ba.BookId).OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<BookAuthor>().HasOne(ba => ba.Author).WithMany(a => a.BookAuthors).HasForeignKey(ba => ba.AuthorId).OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<BookCategory>().HasKey(bc => new { bc.BookId, bc.CategoryId });
-            modelBuilder.Entity<BookCategory>().HasOne(bc => bc.Book).WithMany(b => b.BookCategories).HasForeignKey(bc => bc.BookId).OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<BookCategory>().HasOne(bc => bc.Category).WithMany(c => c.BookCategories).HasForeignKey(bc => bc.CategoryId).OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<UserBookBorrow>().HasKey(ubb => new { ubb.UserId, ubb.BookId });
-            modelBuilder.Entity<UserBookBorrow>().HasOne(ubb => ubb.User).WithMany(u => u.UserBookBorrows).HasForeignKey(ubb => ubb.UserId).OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<UserBookBorrow>().HasOne(ubb => ubb.Book).WithMany(b => b.UserBookBorrows).HasForeignKey(ubb => ubb.BookId).OnDelete(DeleteBehavior.NoAction);
-
-            modelBuilder.Entity<UserBookReserve>().HasKey(ubr => new { ubr.UserId, ubr.BookId });
-            modelBuilder.Entity<UserBookReserve>().HasOne(ubr => ubr.User).WithMany(u => u.UserBookReserves).HasForeignKey(ubr => ubr.UserId).OnDelete(DeleteBehavior.NoAction);
-            modelBuilder.Entity<UserBookReserve>().HasOne(ubr => ubr.Book).WithMany(b => b.UserBookReserves).HasForeignKey(ubr => ubr.BookId).OnDelete(DeleteBehavior.NoAction);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
 }

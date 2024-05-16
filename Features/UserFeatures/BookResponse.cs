b12f85bf941319d57ba648b1ac971fac1aef82cc
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:21:53 +0300

    Request and response added for UserController.

---
+++ Features/UserFeatures/BookResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace library_automation_back_end.Features.UserFeatures
+{
+    public class BookResponse
+    {
+        public required bool Succeeded { get; set; }
+        public required string Message { get; set; }
+    }
+}

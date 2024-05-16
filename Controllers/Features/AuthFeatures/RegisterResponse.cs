0f051eeaca10a02120f13f9777b42eb030f23448
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:24:58 +0300

    AuthFeatures moved to Features folder.

--- Controllers/Features/AuthFeatures/RegisterResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace library_automation_back_end.Controllers.Features.AuthFeatures
-{
-    public class RegisterResponse
-    {
-        public required bool Succeeded { get; set; }
-        public required string Message { get; set; }
-    }
-}

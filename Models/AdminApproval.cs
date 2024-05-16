df4db8f1130930413dc0bc7ee672907aa664e488
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:13:37 +0300

    New model added.

---
+++ Models/AdminApproval.cs
@@ -0,0 +1,13 @@
+﻿namespace library_automation_back_end.Models
+{
+    public class AdminApproval
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(User))] public required int UserId { get; set; }
+        [ForeignKey(nameof(Book))] public required int BookId { get; set; }
+        [ForeignKey(nameof(ApprovalSituation))] public required int ApprovalSituationId { get; set; }
+        public ApprovalSituation? ApprovalSituation { get; set; }
+        public User? User { get; set; }
+        public Book? Book { get; set; }
+    }
+}
---
+++ Models/ApprovalSituation.cs
@@ -0,0 +1,8 @@
+﻿namespace library_automation_back_end.Models
+{
+    public class ApprovalSituation
+    {
+        public int Id { get; set; }
+        public required string Situation { get; set; }
+    }
+}

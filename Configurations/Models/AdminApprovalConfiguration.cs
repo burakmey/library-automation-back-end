3630f88a2e1645740dd9a3a85377b3fce18aea14
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:14:04 +0300

    New model configuration added.

---
+++ Configurations/Models/AdminApprovalConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace library_automation_back_end.Configurations.Models
+{
+    public class AdminApprovalConfiguration : IEntityTypeConfiguration<AdminApproval>
+    {
+        public void Configure(EntityTypeBuilder<AdminApproval> builder)
+        {
+            builder.HasAlternateKey(aa => new { aa.UserId, aa.BookId });
+
+            builder.HasOne(aa => aa.User).WithMany().HasForeignKey(aa => aa.UserId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(aa => aa.Book).WithMany().HasForeignKey(aa => aa.BookId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(aa => aa.ApprovalSituation).WithMany().HasForeignKey(aa => aa.ApprovalSituationId).OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
---
+++ Configurations/Models/ApprovalSituationConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace library_automation_back_end.Configurations.Models
+{
+    public class ApprovalSituationConfiguration : IEntityTypeConfiguration<ApprovalSituation>
+    {
+        public void Configure(EntityTypeBuilder<ApprovalSituation> builder)
+        {
+            builder.HasData(
+            [
+                new() { Id = 1, Situation = "Ödünç" },
+                new() { Id = 2, Situation = "İade" }
+            ]);
+        }
+    }
+}

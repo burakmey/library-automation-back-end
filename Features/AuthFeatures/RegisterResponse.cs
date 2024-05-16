340ac3f6b1a2fc5f316c7784a4d4c51d3786ff7c
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:20:09 +0300

    Request and response added for AuthController.

---
+++ Features/AuthFeatures/RegisterResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace library_automation_back_end.Features.AuthFeatures
+{
+    public class RegisterResponse
+    {
+        public required bool Succeeded { get; set; }
+        public required string Message { get; set; }
+    }
+}

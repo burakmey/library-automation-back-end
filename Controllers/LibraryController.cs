819595b2e3ae52d909e083cdf02072de342b71b0
burakmey <burak_meydan-gs@hotmail.com>
2024-05-16 18:18:03 +0300

    Controller added.

---
+++ Controllers/LibraryController.cs
@@ -0,0 +1,32 @@
+﻿namespace library_automation_back_end.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        readonly LibraryService libraryService;
+
+        public LibraryController(LibraryService libraryService)
+        {
+            this.libraryService = libraryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBook([FromQuery] GetBookRequest request)
+        {
+            GetBookResponse? response = await libraryService.GetBook(request);
+            if (response == null)
+                return BadRequest("Invalid book id!");
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchBook([FromQuery] SearchBookRequest request)
+        {
+            SearchBookResponse? response = await libraryService.GetSearchedBooks(request);
+            if (response == null)
+                return BadRequest("There is no book!");
+            return Ok(response);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿namespace library_automation_back_end.Controllers
+{
+    //[Authorize]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        readonly UserService userService;
+        readonly BookService bookService;
+        public UserController(UserService userService, BookService bookService)
+        {
+            this.userService = userService;
+            this.bookService = bookService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BorrowBook(BookRequest request)
+        {
+            //User? user = await GetUserFromToken();
+            //if (user == null)
+            //    return BadRequest("Invalid refresh token!");
+            BookResponse? response = await bookService.SendBorrowRequest(request, 1);
+            if (!response.Succeeded)
+                return BadRequest(response.Message);
+            return Ok(response.Message);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> CancelBorrowBook(BookRequest request)
+        {
+            //User? user = await GetUserFromToken();
+            //if (user == null)
+            //    return BadRequest("Invalid refresh token!");
+            BookResponse? response = await bookService.DeleteBorrowRequest(request, 1);
+            if (!response.Succeeded)
+                return BadRequest(response.Message);
+            return Ok(response.Message);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReturnBook(BookRequest request)
+        {
+            //User? user = await GetUserFromToken();
+            //if (user == null)
+            //    return BadRequest("Invalid refresh token!");
+            BookResponse? response = await bookService.SendBorrowRequest(request, 1);
+            if (!response.Succeeded)
+                return BadRequest(response.Message);
+            return Ok(response.Message);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> CancelReturnBook(BookRequest request)
+        {
+            //User? user = await GetUserFromToken();
+            //if (user == null)
+            //    return BadRequest("Invalid refresh token!");
+            BookResponse? response = await bookService.DeleteReturnRequest(request, 1);
+            if (!response.Succeeded)
+                return BadRequest(response.Message);
+            return Ok(response.Message);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ReserveBook(BookRequest request)
+        {
+            //User? user = await GetUserFromToken();
+            //if (user == null)
+            //    return BadRequest("Invalid refresh token!");
+            BookResponse? response = await bookService.ReserveBook(request, 1);
+            if (!response.Succeeded)
+                return BadRequest(response.Message);
+            return Ok(response.Message);
+        }
+
+        async Task<User?> GetUserFromToken()
+        {
+            string? refreshToken = Request.Cookies["token"];
+            if (refreshToken == null)
+                return null;
+            User? user = await userService.GetUserWithRefreshToken(refreshToken);
+            return user;
+        }
+    }
+}

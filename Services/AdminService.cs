da923674ca08309836a1322bcd51f0d8bb4f50eb
burakmey <burak_meydan-gs@hotmail.com>
2024-05-17 14:40:05 +0300

    New service added.

---
+++ Services/AdminService.cs
@@ -0,0 +1,75 @@
+﻿namespace library_automation_back_end.Services
+{
+    public class AdminService(DataContext dataContext)
+    {
+        readonly DataContext dataContext = dataContext;
+
+        public async Task<GetAllDesiresResponse?> GetAllDesires()
+        {
+            ICollection<UserDesire>? userDesires = await dataContext.UserDesires.Include(ud => ud.User).Include(ud => ud.Book).ToListAsync();
+            if (userDesires == null)
+                return null;
+
+            GetAllDesiresResponse response = new(userDesires);
+            return response;
+        }
+
+        public async Task<DesireResponse> AcceptBorrow(DesireRequest request)
+        {
+            UserDesire? userDesire = await dataContext.UserDesires.FirstOrDefaultAsync(ud => ud.Id == request.DesireId);
+            if (userDesire == null)
+                return new() { Succeeded = false, Message = "No user desire found for the Id in request." };
+            bool isBookAvailable = await dataContext.Books.AnyAsync(b => b.Id == userDesire.BookId && b.Count > 0);
+            if (!isBookAvailable)
+                return new() { Succeeded = false, Message = "Out of stock or invalid bookId." };
+
+            Book? book = await dataContext.Books.FirstOrDefaultAsync(b => b.Id == userDesire.BookId);
+            book!.Count -= 1;
+            dataContext.UserDesires.Remove(userDesire);
+            DateTime returnDueDate = DateTime.UtcNow.Date.AddDays(8).AddTicks(-1);
+            UserBookBorrow ubb = new()
+            {
+                UserId = userDesire.UserId,
+                BookId = userDesire.BookId,
+                BorrowDate = DateTime.UtcNow,
+                ReturnDueDate = returnDueDate,
+                BorrowSituationId = 1
+            };
+            dataContext.Books.Update(book);
+            dataContext.UserBookBorrows.Add(ubb);
+            await dataContext.SaveChangesAsync();
+            return new() { Succeeded = true, Message = "The borrow request has been approved!" };
+        }
+
+        public async Task<DesireResponse> AcceptReturn(DesireRequest request)
+        {
+            UserDesire? userDesire = await dataContext.UserDesires.FirstOrDefaultAsync(ud => ud.Id == request.DesireId);
+            if (userDesire == null)
+                return new() { Succeeded = false, Message = "No user desire found for the Id in request." };
+            bool isBookAvailable = await dataContext.Books.AnyAsync(b => b.Id == userDesire.BookId);
+            if (!isBookAvailable)
+                return new() { Succeeded = false, Message = "Invalid bookId." };
+
+            Book? book = await dataContext.Books.FirstOrDefaultAsync(b => b.Id == userDesire.BookId);
+            book!.Count += 1;
+            dataContext.UserDesires.Remove(userDesire);
+            UserBookBorrow? ubb = await dataContext.UserBookBorrows.FirstOrDefaultAsync(ubb => ubb.UserId == userDesire.UserId && ubb.BookId == userDesire.BookId);
+            ubb!.ReturnDate = DateTime.Now;
+            ubb!.BorrowSituationId = 2;
+            dataContext.UserBookBorrows.Update(ubb);
+            await dataContext.SaveChangesAsync();
+            return new() { Succeeded = true, Message = "The return request has been approved!" };
+        }
+
+        public async Task<DesireResponse> RejectDesire(DesireRequest request)
+        {
+            UserDesire? userDesire = await dataContext.UserDesires.FirstOrDefaultAsync(ud => ud.Id == request.DesireId);
+            if (userDesire == null)
+                return new() { Succeeded = false, Message = "No user desire found for the Id in request." };
+
+            dataContext.UserDesires.Remove(userDesire);
+            await dataContext.SaveChangesAsync();
+            return new() { Succeeded = true, Message = "The request has been rejected!" };
+        }
+    }
+}
---
+++ Services/AuthService.cs
@@ -0,0 +1,83 @@
+﻿using System.Security.Cryptography;
+
+namespace library_automation_back_end.Services
+{
+    public class AuthService(IConfiguration configuration, DataContext dataContext)
+    {
+        readonly IConfiguration configuration = configuration;
+        readonly DataContext dataContext = dataContext;
+
+        public async Task<RegisterResponse> CreateUser(RegisterRequest request)
+        {
+            if (dataContext.Users.Any(user => user.Email == request.Email))
+                return new() { Succeeded = false, Message = "Email already exists." };
+
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            User user = new()
+            {
+                Email = request.Email,
+                Name = request.Name,
+                Surname = request.Surname,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                CountryId = request.CountyId,
+                RegisteredAt = DateTime.Now,
+                RoleId = 2,
+            };
+            dataContext.Users.Add(user);
+            await dataContext.SaveChangesAsync();
+            return new() { Succeeded = true, Message = "User created!" };
+        }
+
+        public async Task<User?> GetUser(LoginRequest request)
+        {
+            User? user = await dataContext.Users.FirstOrDefaultAsync(user => user.Email == request.Email);
+            if (user == null || !VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
+                return null;
+
+            await dataContext.Entry(user).Reference(u => u.Country).LoadAsync();
+            await dataContext.Entry(user).Reference(u => u.Role).LoadAsync();
+            if (user.Fines != null) await dataContext.Entry(user).Collection(u => u.Fines!).LoadAsync();
+            if (user.UserBookBorrows != null) await dataContext.Entry(user).Collection(u => u.UserBookBorrows!)
+                .Query().Where(ubb => ubb.BorrowSituationId == 1).Include(ubb => ubb.Book).LoadAsync();
+            if (user.UserBookReserves != null) await dataContext.Entry(user).Collection(u => u.UserBookReserves!)
+                .Query().Where(ubr => ubr.ReserveSituationId == 1).Include(ubr => ubr.Book).LoadAsync();
+            return user;
+        }
+        public async Task<User?> GetUserWithRefreshToken(string refreshToken)
+        {
+            User? user = await dataContext.Users.FirstOrDefaultAsync(user => user.RefreshToken == refreshToken);
+            if (user == null || user.RefreshTokenEndDate < DateTime.UtcNow)
+                return null;
+
+            await dataContext.Entry(user).Reference(u => u.Country).LoadAsync();
+            await dataContext.Entry(user).Reference(u => u.Role).LoadAsync();
+            if (user.Fines != null) await dataContext.Entry(user).Collection(u => u.Fines!).LoadAsync();
+            if (user.UserBookBorrows != null) await dataContext.Entry(user).Collection(u => u.UserBookBorrows!)
+                .Query().Where(ubb => ubb.BorrowSituationId == 1).Include(ubb => ubb.Book).LoadAsync();
+            if (user.UserBookReserves != null) await dataContext.Entry(user).Collection(u => u.UserBookReserves!)
+                .Query().Where(ubr => ubr.ReserveSituationId == 1).Include(ubr => ubr.Book).LoadAsync();
+            return user;
+        }
+        public async Task UpdateRefreshToken(User user, Token token)
+        {
+            int additionalMinute = int.Parse(configuration["Token:RefreshTokenMinute"] ?? throw new Exception("RefreshTokenMinute not found!"));
+            user.RefreshToken = token.RefreshToken;
+            user.RefreshTokenEndDate = token.Expiration.AddMinutes(additionalMinute);
+            dataContext.Users.Update(user);
+            await dataContext.SaveChangesAsync();
+        }
+        static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+        static bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512(passwordSalt);
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            return computedHash.SequenceEqual(passwordHash);
+        }
+    }
+}

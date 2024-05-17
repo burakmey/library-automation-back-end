ee4290b52de607ddbf1638245470fbb525b0aaab
burakmey <burak_meydan-gs@hotmail.com>
2024-05-17 14:35:56 +0300

    DbSet names modified.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,8 +4,8 @@ namespace library_automation_back_end.Data
 {
     public class DataContext : DbContext
     {
-        public DbSet<AdminApproval> AdminApprovals { get; set; }
-        public DbSet<ApprovalSituation> ApprovalSituations { get; set; }
+        public DbSet<UserDesire> UserDesires { get; set; }
+        public DbSet<DesireSituation> DesireSituations { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<BookAuthor> BookAuthors { get; set; }

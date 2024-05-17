381d99a89ae93685b16e1021b3bf312252f95884
burakmey <burak_meydan-gs@hotmail.com>
2024-05-17 14:34:38 +0300

    New configuration added.

---
+++ Configurations/Models/UserDesireConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace library_automation_back_end.Configurations.Models
+{
+    public class UserDesireConfiguration : IEntityTypeConfiguration<UserDesire>
+    {
+        public void Configure(EntityTypeBuilder<UserDesire> builder)
+        {
+            builder.HasAlternateKey(ud => new { ud.UserId, ud.BookId });
+
+            builder.HasOne(ud => ud.User).WithMany().HasForeignKey(ud => ud.UserId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(ud => ud.Book).WithMany().HasForeignKey(ud => ud.BookId).OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(ud => ud.DesireSituation).WithMany().HasForeignKey(ud => ud.DesireSituationId).OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}

6b29a38b767c3b816ff14877246acc58f5a8c5af
burakmey <burak_meydan-gs@hotmail.com>
2024-05-17 14:40:28 +0300

    Admin features modified.

---
+++ Features/AdminFeatures/GetAllDesiresResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace library_automation_back_end.Features.AdminFeatures
+{
+    public class GetAllDesiresResponse(ICollection<UserDesire> userDesires)
+    {
+        public ICollection<UserDesireResponse> UserDesireResponses { get; set; } = userDesires.Select(ud => new UserDesireResponse(ud)).ToList();
+    }
+}
---
+++ Features/AdminFeatures/UserDesireResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace library_automation_back_end.Features.AdminFeatures
+{
+    public class UserDesireResponse(UserDesire userDesire)
+    {
+        public int Id { get; set; } = userDesire.Id;
+        public string UserName { get; set; } = userDesire.User!.Name + userDesire.User.Surname;
+        public string BookName { get; set; } = userDesire.Book!.Name;
+        public int DesireSituationId { get; set; } = userDesire.DesireSituationId;
+        public string DesireSituation { get; set; } = userDesire.DesireSituation!.Situation;
+    }
+}

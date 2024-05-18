1742dbf5eb66136319c82a7defda791e07adbf9a
burakmey <burak_meydan-gs@hotmail.com>
2024-05-18 20:35:29 +0300

    Enums for situations added.

---
+++ Configurations/Enums.cs
@@ -0,0 +1,23 @@
+﻿namespace library_automation_back_end.Configurations
+{
+    public enum BorrowSituationEnum
+    {
+        Borrowed = 1,
+        Returned,
+        TimeOut
+    }
+    public enum ReserveSituationEnum
+    {
+        Waiting = 1,
+        Borrowed,
+        TimeOut
+    }
+
+    public enum DesireSituationEnum
+    {
+        Borrow = 1,
+        ReserveBorrow,
+        Return
+    }
+
+}

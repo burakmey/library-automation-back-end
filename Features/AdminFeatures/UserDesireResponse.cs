4430254aa527b4e4031ae652397f9551c4851575
burakmey <burak_meydan-gs@hotmail.com>
2024-05-18 20:36:16 +0300

    UserDesireResponse modified.

--- Features/AdminFeatures/UserDesireResponse.cs
+++ Features/AdminFeatures/UserDesireResponse.cs
@@ -3,9 +3,8 @@
     public class UserDesireResponse(UserDesire userDesire)
     {
         public int Id { get; set; } = userDesire.Id;
-        public string UserName { get; set; } = userDesire.User!.Name + userDesire.User.Surname;
+        public string UserName { get; set; } = userDesire.User!.Name + " " + userDesire.User.Surname;
         public string BookName { get; set; } = userDesire.Book!.Name;
-        public int DesireSituationId { get; set; } = userDesire.DesireSituationId;
         public string DesireSituation { get; set; } = userDesire.DesireSituation!.Situation;
     }
 }

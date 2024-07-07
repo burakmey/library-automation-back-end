551c5dccc1c8d12fb27287d1cf062d07e11db7c5
burakmey <burak_meydan-gs@hotmail.com>
2024-07-08 00:38:16 +0300

    UserDesireResponse feature added.

---
+++ Features/AuthFeatures/UserDesireResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace library_automation_back_end.Features.AuthFeatures
+{
+    public class UserDesireResponse(UserDesire userDesire)
+    {
+        public int Id { get; set; } = userDesire.Id;
+        public string BookName { get; set; } = userDesire.Book!.Name;
+        public string Situation { get; set; } = userDesire.DesireSituation!.Situation;
+    }
+}

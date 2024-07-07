1d325302313a970b3dc3ac4dcbb8ed7dc8969324
burakmey <burak_meydan-gs@hotmail.com>
2024-07-08 00:37:40 +0300

    Borrow and Reserve responses modified.

--- Features/AuthFeatures/UserBookBorrowResponse.cs
+++ Features/AuthFeatures/UserBookBorrowResponse.cs
@@ -3,8 +3,10 @@
     public class UserBookBorrowResponse(UserBookBorrow userBookBorrow)
     {
         public DateTime BorrowDate { get; set; } = userBookBorrow.BorrowDate;
+        public DateTime? ReturnDate { get; set; } = userBookBorrow.ReturnDate;
         public DateTime ReturnDueDate { get; set; } = userBookBorrow.ReturnDueDate;
         public string BorrowSituation { get; set; } = userBookBorrow.BorrowSituation!.Situation;
         public string BookName { get; set; } = userBookBorrow.Book!.Name;
+        public int BookId { get; set; } = userBookBorrow.Book!.Id;
     }
 }
--- Features/AuthFeatures/UserBookReserveResponse.cs
+++ Features/AuthFeatures/UserBookReserveResponse.cs
@@ -3,8 +3,11 @@
     public class UserBookReserveResponse(UserBookReserve userBookReserve)
     {
         public DateTime ReserveDate { get; set; } = userBookReserve.ReserveDate;
+        public DateTime? BorrowDate { get; set; } = userBookReserve.BorrowDate;
         public DateTime ReserveDueDate { get; set; } = userBookReserve.ReserveDueDate;
         public string ReserveSituation { get; set; } = userBookReserve.ReserveSituation!.Situation;
         public string BookName { get; set; } = userBookReserve.Book!.Name;
+        public int BookId { get; set; } = userBookReserve.Book!.Id;
+
     }
 }

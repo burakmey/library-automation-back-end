8fa84ad367a65143b948045c5afae07fd95fbd26
burakmey <burak_meydan-gs@hotmail.com>
2024-07-08 00:36:54 +0300

    UserDesires added to LoginResponse.

--- Features/AuthFeatures/LoginResponse.cs
+++ Features/AuthFeatures/LoginResponse.cs
@@ -1,6 +1,6 @@
 ﻿namespace library_automation_back_end.Features.AuthFeatures
 {
-    public class LoginResponse(User user, Token token)
+    public class LoginResponse(User user, ICollection<UserDesire>? userDesires, Token token)
     {
         public string AccessToken { get; set; } = token.AccessToken;
         public string Email { get; set; } = user.Email;
@@ -12,5 +12,6 @@
         public ICollection<Fine>? Fines { get; set; } = user.Fines;
         public ICollection<UserBookBorrowResponse>? UserBookBorrows { get; set; } = user.UserBookBorrows?.Select(ubb => new UserBookBorrowResponse(ubb)).ToList();
         public ICollection<UserBookReserveResponse>? UserBookReserves { get; set; } = user.UserBookReserves?.Select(ubr => new UserBookReserveResponse(ubr)).ToList();
+        public ICollection<UserDesireResponse>? Desires { get; set; } = userDesires?.Select(ud => new UserDesireResponse(ud)).ToList();
     }
 }

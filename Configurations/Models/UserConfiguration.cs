a393426e8452cd06bceb2c843238d0a33d61f5d2
burakmey <burak_meydan-gs@hotmail.com>
2024-07-15 02:03:03 +0300

    UserConfiguration added.

---
+++ Configurations/Models/UserConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace library_automation_back_end.Configurations.Models
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasData(
+                [
+                    //new() { Id = 1, Email = "admin@hotmail.com", Name = "AdminName", Surname = "AdminSurname", PasswordHash = [], PasswordSalt = [], RegisteredAt = DateTime.UtcNow, CountryId = 11, RoleId = 1 }
+                ]);
+        }
+    }
+}

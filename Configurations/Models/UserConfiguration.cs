ea9f0096100b97d8e5ea1742386a6a1888aa2901
burakmey <burak_meydan-gs@hotmail.com>
2024-07-15 14:38:54 +0300

    Seed data added for admin.

--- Configurations/Models/UserConfiguration.cs
+++ Configurations/Models/UserConfiguration.cs
@@ -1,13 +1,30 @@
-﻿namespace library_automation_back_end.Configurations.Models
+﻿using System.Security.Cryptography;
+
+namespace library_automation_back_end.Configurations.Models
 {
-    public class UserConfiguration : IEntityTypeConfiguration<User>
+    public class UserConfiguration(IConfiguration configuration) : IEntityTypeConfiguration<User>
     {
+        readonly IConfiguration configuration = configuration;
+
         public void Configure(EntityTypeBuilder<User> builder)
         {
+            // Get email and create password for admin.
+            string adminEmail = configuration["ADMIN_EMAIL"] ?? throw new Exception("ADMIN_EMAIL not found!");
+            string adminPassword = configuration["ADMIN_PASSWORD"] ?? throw new Exception("ADMIN_PASSWORD not found!");
+            CreatePasswordHash(adminPassword, out byte[] passwordHash, out byte[] passwordSalt);
+
             builder.HasData(
                 [
-                    //new() { Id = 1, Email = "admin@hotmail.com", Name = "AdminName", Surname = "AdminSurname", PasswordHash = [], PasswordSalt = [], RegisteredAt = DateTime.UtcNow, CountryId = 11, RoleId = 1 }
+                    new() { Id = 1, Email = adminEmail, Name = "AdminName", Surname = "AdminSurname", PasswordHash = passwordHash, PasswordSalt = passwordSalt, RegisteredAt = DateTime.UtcNow, CountryId = 11, RoleId = 1 }
                 ]);
         }
+
+        static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
     }
+
 }

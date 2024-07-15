22c8d10b7389572e4ec2ca0edd04de10e954d801
burakmey <burak_meydan-gs@hotmail.com>
2024-07-16 00:37:39 +0300

    LibraryService modified.

--- Services/LibraryService.cs
+++ Services/LibraryService.cs
@@ -1,4 +1,7 @@
-﻿namespace library_automation_back_end.Services
+﻿using library_automation_back_end.Features.FeaturesLibrary.Requests;
+using library_automation_back_end.Features.FeaturesLibrary.Responses;
+
+namespace library_automation_back_end.Services
 {
     public class LibraryService(DataContext dataContext)
     {
@@ -6,28 +9,30 @@
 
         public async Task<GetBookResponse?> GetBook(GetBookRequest request)
         {
-            Book? book = await dataContext.Books.FirstOrDefaultAsync(book => book.Id == request.BookId);
+            Book? book = await dataContext.Books.Include(b => b.Language).Include(b => b.Publisher).Include(b => b.BookAuthors!).ThenInclude(ba => ba.Author)
+                .Include(b => b.BookCategories!).ThenInclude(bc => bc.Category).FirstOrDefaultAsync(b => b.Id == request.BookId);
             if (book == null)
                 return null;
 
-            await dataContext.Entry(book).Reference(b => b.Language).LoadAsync();
-            await dataContext.Entry(book).Reference(b => b.Publisher).LoadAsync();
-            await dataContext.Entry(book).Collection(b => b.BookAuthors!).LoadAsync();
-            await dataContext.Entry(book).Collection(b => b.BookCategories!).LoadAsync();
             GetBookResponse response = new(book);
             return response;
         }
 
         public async Task<SearchResultResponse?> GetSearchedBooks(SearchBookRequest request)
         {
-            IQueryable<Book> query = dataContext.Books.Include(b => b.BookAuthors).Include(b => b.Publisher).Include(b => b.Language).Include(b => b.BookCategories);
+            IQueryable<Book> query = dataContext.Books.Include(b => b.Publisher).Include(b => b.Language)
+                .Include(b => b.BookAuthors!).ThenInclude(ba => ba.Author).Include(b => b.BookCategories!).ThenInclude(bc => bc.Category);
+
             if (!string.IsNullOrEmpty(request.Search))
-                query = query.Where(b => b.Name.Contains(request.Search) ||
-                b.BookAuthors!.Any(ba => ba.Author!.Name.Contains(request.Search) ||
-                b.Publisher!.Name.Contains(request.Search)));
+            {
+                string search = request.Search.ToLower();
+                query = query.Where(b => b.Name.ToLower().Contains(search) || b.BookAuthors!.Any(ba => ba.Author!.Name.ToLower().Contains(search)) ||
+                    b.Publisher!.Name.ToLower().Contains(search) || b.BookCategories!.Any(bc => bc.Category!.Name.ToLower().Contains(search)));
+            }
             int totalCount = await query.CountAsync();
             if (totalCount == 0)
                 return null;
+
             ICollection<Book> books = await query.Skip(request.Page * request.Size).Take(request.Size).ToListAsync();
             SearchResultResponse response = new(totalCount, books);
             return response;

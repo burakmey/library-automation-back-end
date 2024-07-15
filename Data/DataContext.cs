5454215aee0e9a4ba51f2a711bf415b694695926
burakmey <burak_meydan-gs@hotmail.com>
2024-07-15 14:40:26 +0300

    Configuration added to access ADMIN_EMAIL and ADMIN_PASSWORD in UserConfiguration.

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+﻿using library_automation_back_end.Configurations.Models;
+using System.Reflection;
 
 namespace library_automation_back_end.Data
 {
-    public class DataContext : DbContext
+    public class DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : DbContext(options)
     {
         public DbSet<UserDesire> UserDesires { get; set; }
         public DbSet<DesireSituation> DesireSituations { get; set; }
@@ -21,9 +22,12 @@ namespace library_automation_back_end.Data
         public DbSet<User> Users { get; set; }
         public DbSet<UserBookBorrow> UserBookBorrows { get; set; }
         public DbSet<UserBookReserve> UserBookReserves { get; set; }
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        readonly IConfiguration configuration = configuration;
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new UserConfiguration(configuration));
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
         }
     }
